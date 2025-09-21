903634083a126e3b0426287d415e630cdfd72cc2
Gerardo Flores <geraflores90@gmail.com>
2025-09-21 17:21:38 -0600

    Se añadió el archivo HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main (String [] args) {
+        System.out.println("Hola Mundo!");
+        System.out.println("Este es un cambio en el código hecho de manera local");
+        System.out.println("Este es otro cambio en el código hecho de manera local"); // Cambio en el código.
+    }
+}
